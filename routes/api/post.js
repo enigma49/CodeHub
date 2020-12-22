6d693fcc8ebbcd88b2e30e4008cbc6af036ad1e7
HamzaSa <hamza.shaikh@greatmanagerinstitute.com>
2020-12-23 02:07:44 +0530

    Post Api Routes

--- routes/api/post.js
+++ routes/api/post.js
@@ -1,9 +1,214 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
+const { check, validationResult } = require("express-validator");
+const auth = require("../../middleware/auth");
+const Post = require("../../models/post");
+const User = require("../../models/user");
+const Profile = require("../../models/profile");
 
-//@route GET api/routes
-//@desc Test route
-//@aaccess public
-router.get("/", (req,res)=>{res.send("post route works")});
+//@route POST api/post
+//@desc add new posts
+//@access private
+router.post(
+  "/",
+  [auth, check("text", "Text cannot be empty").not().isEmpty()],
+  async (req, res) => {
+    const error = validationResult(req);
+    if (!error.isEmpty()) {
+      res.status(400).json({ error: error.array() });
+    }
+    try {
+      const user = await User.findById(req.user.id).select("-password");
 
+      const newPost = new Post({
+        text: req.body.text,
+        name: user.name,
+        avatar: user.avatar,
+        user: req.user.id,
+      });
+
+      const post = await newPost.save();
+      res.json(post);
+    } catch (err) {
+      console.log(err.message);
+      res.status(500).json("Server Error");
+    }
+  }
+);
+
+//@route GET api/post
+//@desc get all posts
+//@access private
+
+router.get("/", auth, async (req, res) => {
+  try {
+    const posts = await Post.find().sort({ date: -1 });
+    res.json(posts);
+  } catch (err) {
+    console.log(err.message);
+    res.status(500).send("Server Error");
+  }
+});
+
+//@route GET api/post/:id
+//@desc get posts by id
+//@access private
+
+router.get("/:id", auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    if (!post) {
+      return res.status(404).json("Post Not Found");
+    }
+    res.json(post);
+  } catch (err) {
+    console.log(err.message);
+    if (err.kind === "ObjectId") {
+      return res.status(404).json("Post Not Found");
+    }
+    res.status(500).send("Server Error");
+  }
+});
+
+//@route DELETE api/post/:id
+//@desc delete a post
+//@access private
+
+router.delete("/:id", auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+
+    if (!post) {
+      return res.status(404).json("Post Not Found");
+    }
+
+    //Check if user is the owner
+    if (post.user.toString() !== req.user.id) {
+      return res.status(401).json({ msg: "User not authorised" });
+    }
+    await post.remove();
+    res.json({ msg: "Post Removed" });
+  } catch (err) {
+    console.log(err.message);
+    if (err.kind === "ObjectId") {
+      return res.status(404).json("Post Not Found");
+    }
+    res.status(500).send("Server Error");
+  }
+});
+
+//@route PUT api/post/like/:id
+//@desc like a post
+//@access private
+router.put("/like/:id", auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+
+    //Check if post is already liked
+    if (
+      post.likes.filter((like) => like.user.toString() === req.user.id).length >
+      0
+    ) {
+      return res.status(400).json({ msg: "Post already liked" });
+    }
+
+    post.likes.unshift({ user: req.user.id });
+    await post.save();
+    res.json(post.likes);
+  } catch (err) {
+    console.log(err.message);
+    res.status(500).send("Server Error");
+  }
+});
+
+//@route PUT api/post/unlike/:id
+//@desc like a post
+//@access private
+router.put("/unlike/:id", auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+
+    //Check if post is already liked
+    if (
+      post.likes.filter((like) => like.user.toString() === req.user.id)
+        .length === 0
+    ) {
+      return res.status(400).json({ msg: "Post is not liked yet" });
+    }
+
+    //Get Remove Index
+    const removeIndex = post.likes
+      .map((like) => like.user.toString())
+      .indexOf(req.user.id);
+    post.likes.splice(removeIndex, 1);
+    await post.save();
+    res.json(post.likes);
+  } catch (err) {
+    console.log(err.message);
+    res.status(500).send("Server Error");
+  }
+});
+
+//@route POST api/post/comment/:id
+//@desc add new comments on post
+//@access private
+router.post(
+  "/comment/:id",
+  [auth, check("text", "Text cannot be empty").not().isEmpty()],
+  async (req, res) => {
+    const error = validationResult(req);
+    if (!error.isEmpty()) {
+      res.status(400).json({ error: error.array() });
+    }
+    try {
+      const user = await User.findById(req.user.id).select("-password");
+      const post = await Post.findById(req.params.id);
+      const newComment = {
+        text: req.body.text,
+        name: user.name,
+        avatar: user.avatar,
+        user: req.user.id,
+      };
+
+      post.comments.unshift(newComment);
+      await post.save();
+      res.json(post.comments);
+    } catch (err) {
+      console.log(err.message);
+      res.status(500).json("Server Error");
+    }
+  }
+);
+
+//@route DELETE api/post/comment/:id/:comment_id
+//@desc delete comment on post
+//@access private
+router.delete("/comment/:id/:comment_id", auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+
+    //Get Comment From Post
+    const comment = post.comments.find(
+      (comment) => comment.id === req.params.comment_id
+    );
+
+    //Check if comment exists
+    if (!comment) {
+      return res.status(404).json({ msg: "Comment Does Not Exists" });
+    }
+    if (comment.user.toString() !== req.user.id) {
+      return res.status(401).json({ msg: "Unauthorized User" });
+    }
+    //Get Remove Index
+    const removeIndex = post.comments
+      .map((comment) => comment.user.toString())
+      .indexOf(req.user.id);
+    post.comments.splice(removeIndex, 1);
+    await post.save();
+    res.json(post.comments);
+  } catch (err) {
+    console.log(err.message);
+    res.status(500).send("Server Error");
+  }
+});
 module.exports = router;
