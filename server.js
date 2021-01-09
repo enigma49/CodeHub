0e118bf0f8a0dea0470abd86afc385d642110c9e
HamzaSa <hamza.shaikh@greatmanagerinstitute.com>
2021-01-10 03:03:31 +0530

    Deployment Changes

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const connectToDB = require('./config/db');
 const bodyParser = require('body-parser');
 const app = express();
+const path = require('path');
 
 //Connecting to database
 connectToDB();
@@ -16,6 +17,16 @@ app.use('/api/user', require('./routes/api/user'));
 app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/profile', require('./routes/api/profile'));
 app.use('/api/post', require('./routes/api/post'));
+
+//Serve Static assets in production
+if(process.env.NODE_ENV === 'production'){
+    //set static folder
+    app.use(express.static('client/build'));
+
+    app.get('*',(req,res)=>{
+        res.sendFile(path.resolve(__dirname, 'client', 'build','index.html'));
+    })
+}
 
 PORT = process.env.PORT || 3500;
 app.listen(PORT,()=>{console.log(`Server Started on Port:${PORT}`)});
